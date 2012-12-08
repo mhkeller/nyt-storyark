59dbfaa600935a69c27091d9b189f65c8068a483
Michael Keller <michael.keller@gmail.com>
2012-12-08 14:52:17 -0500

    etc

--- js/app.js
+++ js/app.js
@@ -1,19 +1,25 @@
 (function(){
 	var fetchTerms = function(lame_term){
+		console.log('there')
+		var api_key = encodeURIComponent('8537fd77a66fcdcf309a3f864cec0d77:14:65316364');
+		var request_url = 'http://api.nytimes.com/svc/semantic/v2/concept/search.json?&query='+lame_term+'&api-key='+api_key
+		console.log(request_url);
 		$.ajax({
-		  url: 'api-call',
+		  url: request_url,
+		  dataType: 'JSON',
 		  success: function(data) {
-
+		  	console.log(data);
+		  	
 		  }
 		});
 
 
 	}
 
-	$('#search-term').submit( function(){
-		var term = $('#search-term-input').val();
+	$('#search-term-submit').click( function(){
+		var lame_term = $('#search-term-input').val();
 		fetchTerms(lame_term)
-		return false
+		// return false
 	});
 
 	// Hide input help text on focus
