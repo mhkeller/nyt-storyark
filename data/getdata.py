94a3ed6f72433d23ad8079c17790b98e7ccd95c5
Brian Abelson <brianabelson@gmail.com>
2012-12-08 14:08:36 -0500

    added python script to download example data

---
+++ data/getdata.py
@@ -0,0 +1,25 @@
+import re
+import json
+import sys
+import requests
+
+url = "http://api.nytimes.com/svc/search/v1/article?format=json&query=Damascus+%28Syria%29&fields=date%2C+word_count%2C+title%2C+url&rank=newest&api-key=hackday2012api"
+r = requests.get(url)
+raw_data = r.json
+
+n_pages = raw_data['total'] / 10
+offsets = range(1, n_pages)
+data = raw_data['results']
+print data
+
+for offset in offsets:
+    the_url = url + '&offset=' + str(offset)
+    r = requests.get(the_url)
+    the_data = r.json 
+    data.append(the_data['results'])
+    print "downloading", the_url
+
+with open('data.json', 'wb') as fp:
+    json.dump(data, fp)
+
+
