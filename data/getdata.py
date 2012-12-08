64e839c317f2e2d0adbc0ead44e833b22a0c72da
Brian Abelson <brianabelson@gmail.com>
2012-12-08 14:14:29 -0500

    try again?

--- data/getdata.py
+++ data/getdata.py
@@ -1,6 +1,4 @@
-import re
 import json
-import sys
 import requests
 
 url = "http://api.nytimes.com/svc/search/v1/article?format=json&query=Damascus+%28Syria%29&fields=date%2C+word_count%2C+title%2C+url&rank=newest&api-key=hackday2012api"
@@ -16,7 +14,7 @@ for offset in offsets:
     r = requests.get(the_url)
     the_data = r.json 
     data.append(the_data['results'])
-    print "downloading", the_url
+    print "downloading:", the_url
 
 with open('data.json', 'wb') as fp:
     json.dump(data, fp)
